9fbebb3ca80794cbd873cc1a69833aeebfe60b3e
ugurctaygun <ugurctaygun@gmail.com>
2023-08-22 22:16:20 +0300

    Updates

--- assets/DisplayField.stories-7ce13327.js
+++
@@ -1,7 +0,0 @@
-import{D as o}from"./index-e3dd031b.js";import"./useThemeProps-8d75b814.js";import"./index-76fb7be0.js";import"./_commonjsHelpers-de833af9.js";import"./emotion-use-insertion-effect-with-fallbacks.browser.esm-55b21f7f.js";import"./Box-84ddc883.js";import"./styled-e735bca7.js";import"./Typography-4691d2c3.js";const u={component:o,title:"Design System/Components/Display Field",parameters:{layout:"centered"}},e={args:{label:"Company Name",value:"Arçelik A.Ş"}};var a,r,t;e.parameters={...e.parameters,docs:{...(a=e.parameters)==null?void 0:a.docs,source:{originalSource:`{
-  args: {
-    label: "Company Name",
-    value: 'Arçelik A.Ş'
-  }
-}`,...(t=(r=e.parameters)==null?void 0:r.docs)==null?void 0:t.source}}};const y=["DefaultDisplayField"];export{e as DefaultDisplayField,y as __namedExportsOrder,u as default};
-//# sourceMappingURL=DisplayField.stories-7ce13327.js.map
---
+++ assets/DisplayField.stories-d904f59a.js
@@ -0,0 +1,7 @@
+import{D as o}from"./index-8a89803b.js";import"./useThemeProps-885cf5c5.js";import"./index-76fb7be0.js";import"./_commonjsHelpers-de833af9.js";import"./emotion-use-insertion-effect-with-fallbacks.browser.esm-55b21f7f.js";import"./Box-b5241adb.js";import"./styled-39ccb8f1.js";import"./Typography-a2ca9b1c.js";const u={component:o,title:"Design System/Components/Display Field",parameters:{layout:"centered"}},e={args:{label:"Company Name",value:"Arçelik A.Ş"}};var a,r,t;e.parameters={...e.parameters,docs:{...(a=e.parameters)==null?void 0:a.docs,source:{originalSource:`{
+  args: {
+    label: "Company Name",
+    value: 'Arçelik A.Ş'
+  }
+}`,...(t=(r=e.parameters)==null?void 0:r.docs)==null?void 0:t.source}}};const y=["DefaultDisplayField"];export{e as DefaultDisplayField,y as __namedExportsOrder,u as default};
+//# sourceMappingURL=DisplayField.stories-d904f59a.js.map
