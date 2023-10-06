cb8d86287572b81b35f7e136c3e3594e1b5980b9
ugurctaygun <ugurctaygun@gmail.com>
2023-10-06 22:23:26 +0300

    removed stories

--- .storybook/theme.ts
+++ .storybook/theme.ts
@@ -2,8 +2,5 @@ import { create } from '@storybook/theming/create';
 
 export default create({
   base: 'dark',
-  brandTitle: 'Connecta UI',
-  brandUrl: 'https://example.com',
-  brandImage: (process.env.NODE_ENV === 'production') ? 'logo.png' : '/logo.png',
   brandTarget: '_self',
 });
